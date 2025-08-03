9a16a6a9aaebd47f80800f3ae53b68cb21e7c1c7
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-08-04 04:37:15 +0530

    Create Main.py

---
+++ Quick Mad Libs/Main.py
@@ -0,0 +1,9 @@
+def mad_libs():
+    print("Let’s make a silly story!")
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adj = input("Enter an adjective: ")
+    print(f"Once upon a time, a {adj} {noun} decided to {verb} all day long.")
+
+if __name__ == "__main__":
+    mad_libs()
